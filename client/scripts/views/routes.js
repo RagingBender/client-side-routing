6327d34b07c701267b59f49c12e55b25e1f769bf
jackie <jcqnly@gmail.com>
2018-04-20 20:49:37 -0700

    clarify page routes, add button to view details

--- client/scripts/views/routes.js
+++ client/scripts/views/routes.js
@@ -8,5 +8,6 @@ page('/*', (ctx, next) => {
 page('/', app.bookView.initIndexPage);
 // page('/books/:book_id', app.showOne.initIndexPage);
 page('/new', app.formView.initIndexPage);
+page('/api/v1/books/', (ctx) => app.Book.fetchOne(ctx.params.id).then(app.showOne.initIndexPage));
 
 page.start();
